391b5ab4126e35ebc4e74647694b0052c90392d4
LyrasaurusRose <49761691+LyrasaurusRose@users.noreply.github.com>
2021-09-18 20:51:07 -0600

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,13 @@
+'''
+Some important notes:
+    \033[94m is the shell ANSI escape for BLUE
+    \033[93m is the shell ANSI escape for YELLOW
+    \033[95m is the shell ANSI escape for PURPLE
+    \033[4m is the shell ANSI escape for UNDERLINE
+    \033[0m is the shell ANSI escape for CLEARFORMATTING
+
+    Escape codes retrieved from https://stackoverflow.com/a/287944
+'''
+
+print("\033[94mHello World!\033[0m") # Hello World
+print("\033[93mWelcome to the GitHub of \033[0m\033[95m\033[4mLyrasaurusRose!\033[0m")
